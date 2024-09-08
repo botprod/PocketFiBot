caff69e32781bf0be4818bb142f66db8a1a0c246
Максим Адаев <maximkad2008@gmail.com>
2024-09-09 00:48:34 +0300

    headers fix

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -1,14 +1,16 @@
+from fake_useragent import UserAgent
+
 headers = {
         'accept': '*/*',
         'accept-language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
         'origin': 'https://pocketfi.app',
         'priority': 'u=1, i',
         'referer': 'https://pocketfi.app/',
-        'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Microsoft Edge";v="128", "Microsoft Edge WebView2";v="128"',
+        'sec-ch-ua': '"Android WebView";v=33, "Chromium";v=33, "Not?A_Brand";v="125"',
         'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
+        'sec-ch-ua-platform': '"android"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 15) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.6613.127 Mobile Safari/537.36',
 }
